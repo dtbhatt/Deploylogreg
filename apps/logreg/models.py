77059b6ed792c9a9a36ad0d303f42426e36fc0b9
Dtbhatt <d.bhatt89@gmail.com>
2017-06-30 12:09:34 -0700

    Python Retake 1

--- apps/logreg/models.py
+++ apps/logreg/models.py
@@ -7,91 +7,100 @@ import re, bcrypt
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 class UserManager(models.Manager):
-        def validlogin(self, postData):
-            email = postData["email"]
-            password = postData["password"]
-            errors = []
+    def validlogin(self, postData):
+        email = postData["email"]
+        password = postData["password"]
+        errors = []
 
-            user_list = User.objects.filter(email=email)        
+        user_list = User.objects.filter(email=email)        
 
-            if not user_list: 
-                errors.append("Invalid email and invalid password")
-            else:
-                user = user_list[0]
-                if bcrypt.checkpw(password.encode(), user.password.encode()):
-                      return {"status": True, "data": user_list[0]}
-                else: 
-                    errors.append('wrong password')
-            return {"status": False, "data": errors}  
-            
-        def validregister(self, postData):
-            name = postData["name"]
-            alias = postData["alias"]
-            email = postData['email']
-            password = postData['password']
-            confirmpass = postData['confirmpass']
-            birthdate = postData['birthdate']
+        if not user_list: 
+            errors.append("Invalid email and invalid password")
+        else:
+            user = user_list[0]
+            if bcrypt.checkpw(password.encode(), user.password.encode()):
+                    return {"status": True, "data": user_list[0]}
+            else: 
+                errors.append('wrong password')
+        return {"status": False, "data": errors}  
+        
+    def validregister(self, postData):
+        name = postData["name"]
+        alias = postData["alias"]
+        email = postData['email']
+        password = postData['password']
+        confirmpass = postData['confirmpass']
+        birthdate = postData['birthdate']
+
+        errors = []
+
+        if len(name) < 1:
+            errors.append("Name cannot be empty")
+        elif not name.isalpha():
+            errors.append("Name can only contain leters")
+        
+        if len(alias) < 1:
+            errors.append("Name cannot be empty")
+        elif not alias.isalpha():
+            errors.append("Name can only contain leters")
+        
+        if len(email) < 1:
+            errors.append("Email cannot be empty")
+        elif not EMAIL_REGEX.match(email):
+            errors.append("Email is not valid")
+        
+        if len(password) < 8:
+            errors.append("Password should be at least 8 characters")
+        elif password != confirmpass:
+            errors.append("Password and Confirm password has to be same")
+        
+        if len(confirmpass) < 8:
+            errors.append("Confirm password should be at least 8 characters")
+        elif confirmpass != password:
+            errors.append("Password and Confirm password has to be same")
+        
+        if not birthdate:
+            errors.append("Please input your birthdate")
+
+        
+        exist = User.objects.filter(email=email).exists()
+
+        if exist:
+            errors.append("Already exists")
+        
+        if not errors:
+            mypassword = password.encode()
+            hashed = bcrypt.hashpw(mypassword,bcrypt.gensalt())
+            user = User.objects.create(name=name, alias=alias, email=email, password=hashed, birthdate=birthdate)
+            return {"status": True, "data": user}
+        else:
+            return {"status": False, "data": errors}
+        
+    def addFriend(self, userId, friendId):
+        user = self.get(id=userId)
+        friend = self.get(id=friendId)
+        Friend.objects.create(friendRequest=user, acceptFriend=friend)
+        Friend.objects.create(friendRequest=friend, acceptFriend=user)
     
-            errors = []
-
-            if len(name) < 1:
-                errors.append("Name cannot be empty")
-            elif not name.isalpha():
-                errors.append("Name can only contain leters")
-            
-            if len(alias) < 1:
-                errors.append("Name cannot be empty")
-            elif not alias.isalpha():
-                errors.append("Name can only contain leters")
-            
-            if len(email) < 1:
-                errors.append("Email cannot be empty")
-            elif not EMAIL_REGEX.match(email):
-                errors.append("Email is not valid")
-            
-            if len(password) < 8:
-                errors.append("Password should be at least 8 characters")
-            elif password != confirmpass:
-                errors.append("Password and Confirm password has to be same")
-            
-            if len(confirmpass) < 8:
-                errors.append("Confirm password should be at least 8 characters")
-            elif confirmpass != password:
-                errors.append("Password and Confirm password has to be same")
-            
-            if not birthdate:
-                errors.append("Please input your birthdate")
-
-            
-            exist = User.objects.filter(email=email).exists()
-
-            if exist:
-                errors.append("Already exists")
-            
-            if not errors:
-                mypassword = password.encode()
-                hashed = bcrypt.hashpw(mypassword,bcrypt.gensalt())
-                user = User.objects.create(name=name, alias=alias, email=email, password=hashed, birthdate=birthdate)
-                return {"status": True, "data": user}
-            else:
-                return {"status": False, "data": errors}
-
-class PokeManager(models.Manager):
-        def newpoke(self, UserId):
-            user = User.objects.get(id=UserId)
-            pokers.add(user)
-            return self
+    def removeFriend(self, userId, friendId):
+        user = self.get(id=userId)
+        friend = self.get(id=friendId)
+        removeFriend1 = Friend.objects.get(friendRequest=user, acceptFriend=friend)
+        removeFriend2 = Friend.objects.get(friendRequest=friend, acceptFriend=user)   
+        removeFriend1.delete()
+        removeFriend2.delete()         
 
 class User(models.Model):
-        name = models.CharField(max_length=45)
-        alias = models.CharField(max_length=45)
-        email = models.CharField(max_length=200)
-        password = models.CharField(max_length=100)
-        birthdate = models.DateField(blank=False, null=True)
-        objects = UserManager()
+    name = models.CharField(max_length=45)
+    alias = models.CharField(max_length=45)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=100)
+    birthdate = models.DateField(blank=False, null=True)
+    objects = UserManager()
 
-class Poke(models.Model):
-        pokers = models.ManyToManyField(User, related_name="allpokes")
-        objects = PokeManager()
+class Friend(models.Model):
+    friendRequest = models.ForeignKey(User, related_name="requester")
+    acceptFriend = models.ForeignKey(User, related_name="accepter")
+    objects = UserManager()
 
 # Create your models here.
--- apps/logreg/views.py
+++ apps/logreg/views.py
@@ -1,29 +1,63 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
 
-from .models import User, Poke
+
+from .models import User, Friend
 
 def index(request):
     return render(request, 'logreg/index.html')
 
-def success(request):
-    if "id" in request.session:
-        context = {
-            "user":User.objects.get(id=request.session["id"]),
-            "users":User.objects.all(),
-            "pokes":Poke.objects.all()
-        }
-        return render(request, 'logreg/success.html', context)
-    else:
-        messages.add_message(request, messages.ERROR, "Must be a Registered User!")
-        return redirect('/')
+def friends(request):
+    # if "id" in request.session:
+    #     context = {
+    #         "user":User.objects.get(id=request.session["id"])
+    #     }
+    #     return render(request, 'logreg/friends.html', context)
+    # else:
+    #     messages.add_message(request, messages.ERROR, "Must be a Registered User!")
+    #     return redirect('/')
+    selfie = User.objects.get(id=request.session['id'])
+    try:
+        users = User.objects.all()
+        otherUsers = []
+        for others in users:
+            if (others.id != request.session['id']):
+                otherUsers.append(others)
+    except:
+        users = None
+    
+    try: 
+        friends = Friend.objects.filter(friendRequest=selfie)
+        existFriends = []
+        for friend in friends:
+            existFriends.append(friend.acceptFriend)
+        existOthers = []
+        for others in otherUsers:
+            if (others not in existFriends):
+                existOthers.append(others)
+    except:
+        friends = None
+    
+    context = {
+        'selfie': selfie,
+        'users': existOthers,
+        'friends': existFriends
+    }
+    return render(request, 'logreg/friends.html', context)
+
+def user(request, id):
+    profile = User.objects.get(id=id)
+    context = {
+        "user": profile
+    }
+    return render(request, 'logreg/user.html', context)
 
 def login(request):
     res = User.objects.validlogin(request.POST)
     if res["status"]:
         # set id in session
         request.session["id"] = res["data"].id
-        return redirect('/success')
+        return redirect('/friends')
     
     for error in res["data"]:
         messages.error(request, error) 
@@ -36,16 +70,20 @@ def register(request):
     if res["status"]:
         # set id in session
         request.session["id"] = res["data"].id
-        return redirect('/success')
+        return redirect('/friends')
     
     for error in res["data"]:
         messages.error(request, error) 
 
     return redirect('/')
 
-def newpoke(request, id):
-    poke = Poke.objects.newpoke(id, request.session['id'])
-    return redirect('/success')
+def addfriend(request, id):
+    User.objects.addFriend(request.session["id"], id)
+    return redirect('/friends')
+
+def removefriend(request, id):
+    User.objects.removeFriend(request.session["id"], id)
+    return redirect('/friends')
 
 def logout(request):
     request.session.pop('id')
